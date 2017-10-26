1a987d6f3fbb94e4f9947122e490bcd0cd187f65
Pixelformater <pixelformater@gmail.com>
2017-10-26 09:28:32 +0100

    Added the option to download each game and DLC to their own folders.

--- NPS/Options.cs
+++ NPS/Options.cs
@@ -31,6 +31,7 @@ namespace NPS
             checkBox1.Checked = Settings.Instance.deleteAfterUnpack;
             numericUpDown1.Value = Settings.Instance.simultaneousDl;
             textBox3.Text = Settings.Instance.PSMUri;
+            checkBox3.Checked = Settings.Instance.downloadToGameDir;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -126,5 +127,11 @@ namespace NPS
                 }
             }
         }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings.Instance.downloadToGameDir = checkBox3.Checked;
+        }
+
     }
 }
